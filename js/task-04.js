9c55d1c8868c83a6c36555c057830552a518103c
Eduard <mefoed@gmail.com>
2023-05-11 20:17:03 +0300

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,22 @@
+let counterValue = 0;
+
+const decrementBtnRef = document.querySelector('button[data-action="decrement"]');
+const incrementBtnRef = document.querySelector('button[data-action="increment"]');
+const valueRef = document.querySelector("#value");
+
+decrementBtnRef.addEventListener('click', handleDecrementBtnClick);
+incrementBtnRef.addEventListener('click', handleIncrementBtnClick)
+
+function handleDecrementBtnClick () {
+    counterValue -= 1;
+    // console.log(counterValue);
+    valueRef.textContent = counterValue;
+}
+   
+function handleIncrementBtnClick() {
+    counterValue += 1;
+    // console.log(counterValue);
+    valueRef.textContent = counterValue;
+}
+
+
