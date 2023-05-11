47add66d02a3d013282132214388ac7520400a7d
Eduard <mefoed@gmail.com>
2023-05-11 10:40:04 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = ingredients.map(ingredient => {
+  const liEl = document.createElement("li");
+  
+  liEl.classList.add("item");
+  liEl.textContent = ingredient;
+
+  return liEl;
+});
+
+const ingredientsEl = document.querySelector("#ingredients");
+// console.log(ingredientsEl);
+ingredientsEl.append(...listEl);
+
+
+
