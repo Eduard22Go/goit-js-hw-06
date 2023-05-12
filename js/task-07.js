e857e1b59bdc49ebcb74eebbd599ba6715ac91a7
Eduard <mefoed@gmail.com>
2023-05-12 19:05:55 +0300

    Update task-07.js

---
+++ js/task-07.js
@@ -0,0 +1,9 @@
+const inputRef = document.querySelector('input');
+
+inputRef.addEventListener('input', inputHandler)
+
+function inputHandler(event) {
+    const spanRef = document.querySelector('span');
+    spanRef.style.fontSize = event.currentTarget.value + 'px';
+}
+
