2f65b2b0cc1db413289758baec6ded1e938e4bad
Eduard <mefoed@gmail.com>
2023-05-15 13:29:44 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const formRef = document.querySelector('.login-form');
+
+formRef.addEventListener('submit', formSubmitHandler)
+
+function formSubmitHandler(event) {
+    event.preventDefault();
+    const { elements: { email, password }} = event.currentTarget;
+    // console.log(event.currentTarget.elemets);
+
+    if (email.value === "" || password.value === "") {
+      return alert("Заповніть усі поля!");
+    }
+    
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
 
