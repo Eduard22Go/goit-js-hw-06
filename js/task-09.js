387201cc9b38953392f2bf82f787ce992a42a3fc
Eduard <mefoed@gmail.com>
2023-05-16 21:34:36 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,15 +1,32 @@
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+// const buttonRef = document.querySelector('.change-color');
+// const spanRef = document.querySelector('.color')
+
+// buttonRef.addEventListener('click', buttonClickHandler);
+
+// function buttonClickHandler() {
+//   const body = document.querySelector('body');
+//   body.style.backgroundColor = getRandomHexColor();
+//   spanRef.textContent = getRandomHexColor();
+// }
+
+// ---
+const refs = {
+  chengeColor: document.querySelector(".change-color"),
+  selectedColor: document.querySelector(".color"),
+};
+
+refs.chengeColor.addEventListener("click", onColorChange);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const buttonRef = document.querySelector('.change-color');
-const spanRef = document.querySelector('.color')
-
-buttonRef.addEventListener('click', buttonClickHandler);
-
-function buttonClickHandler() {
-  const body = document.querySelector('body');
-  body.style.backgroundColor = getRandomHexColor();
-  spanRef.textContent = getRandomHexColor();
+function onColorChange(event) {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  refs.selectedColor.textContent = color;
 }
-
